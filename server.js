2ac97161a76e13ac2ccb5e9af0010868541acb30
Rahul Krishna <raahulkrsna@gmail.com>
2016-02-05 07:51:05 -0700

    Initialize repo

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var app = express();
+const PORT = 3000;
+
+
+var middleware = {
+	requireAuthentication: function(req, res, next) {
+		console.log("private route hit!");
+		next();
+	},
+	logger: function(req, res, next) {
+		console.log("Request: " + new Date().toString() + " " + req.method + " " + req.originalUrl);
+		next();
+	}
+}
+
+// app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+app.get("/about", middleware.requireAuthentication, function(request, response) {
+	response.send("About Us: Rahul Krishna Vasantham")
+})
+
+// allow the public folder for access
+app.use(express.static(__dirname+"/public"))
+
+// console.log(__dirname); // Get the root directory of the webserver
+
+app.listen(PORT, function() {
+	console.log("Express server started and listening on port "+PORT+"!");
+});
+
+
+// app.get("/",function (request, response) {
+// 	response.send("Hello Express!");
+// });
